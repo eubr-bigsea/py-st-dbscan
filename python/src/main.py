a9cd70cb45470ceb9b58f67e56276f0c96624ce6
lucasmsp <lucasmsp@gmail.com>
2017-10-25 16:01:41 -0200

    adding a main file

--- python/src/main.py
+++ python/src/main.py
@@ -7,30 +7,52 @@ import argparse
 from stdbscan import *
 
 
+"""
+    The minimal command to run this algorithm is:
+    $ python main.py -f sample.csv
+
+    Or could be executed with advanced configurations:
+    $ python main.py -f sample.csv -p 5 -s 500 -t 60
+
+    In the current momment, the dataset must have the
+    'latitude', 'longitude' and 'date_time' columns, but
+    if you want, can be easily changed.
+
+"""
+
 def parse_dates(x):
     return datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f')
 
 def main():
     import time
     start= time.time()
-    parser = argparse.ArgumentParser(description='ST-DBSCAN  implementation with COMPSs')
+    parser = argparse.ArgumentParser(description='ST-DBSCAN in Python')
     parser.add_argument('-f','--filename', help='Name of the file', required=True)
+    parser.add_argument('-p','--minPts',   help='Minimum number of points', required=False, type=int, default=15)
+    parser.add_argument('-s','--spatial',  help='Spatial Threshold (in meters)', required=False, type=float, default=500)
+    parser.add_argument('-t','--temporal', help='Temporal Threshold (in seconds)', required=False, type=float, default=60)
+    args = parser.parse_args()
     args = parser.parse_args()
 
-    filename = args.filename
-    df = pd.read_csv(filename,sep=";",converters={'DATATIME': parse_dates})
+    filename    = args.filename
+    minPts      = args.minPts
+    spatial_threshold   = args.spatial
+    temporal_threshold  = args.temporal
+    args = parser.parse_args()
 
-    spatial_threshold   = 500   #in meters
-    temporal_threshold  = 60    #in seconds
-    minPts              = 5
 
-    
-    result = ST_DBSCAN(df,numPoints, spatial_threshold, temporal_threshold, minPts)
+    df = pd.read_csv(filename,sep=";",converters={'date_time': parse_dates})
+
+    result = st_dbscan(df, spatial_threshold, temporal_threshold, minPts)
     print "Time Elapsed: {} seconds".format(time.time()-start)
 
     timestr = time.strftime("%Y%m%d-%H%M%S")
 
-    result_df.to_csv("result_{}_{}_{}_{}.csv".format(spatial_threshold,temporal_threshold,minPts,timestr))
+    output_name = "result_{}_{}_{}_{}.csv".format(spatial_threshold,
+                                                  temporal_threshold,
+                                                  minPts,
+                                                  timestr)
+    result.to_csv(output_name,index=False,sep=';')
 
 
 
