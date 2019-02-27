148cd43c318a9ee8dfa512fbfd50643d5c214025
Xiaoqi_Shen <Xiaoqi_Shen@outlook.com>
2019-02-27 10:17:42 +0800

    by transforming dataframe into numpy.arrays, greatly improve the runtime especially when the data is very large, the dataframe my use several times time to search the column and row by index transform the lon and lat firstly, and use x y to calculate distance for the truth that earth is irregular sphere, but now need epsg(the test_st_dbscan.py use epsg by my supposition, because I don't know the true one)

--- python/src/stdbscan.py
+++ python/src/stdbscan.py
@@ -1,11 +1,28 @@
 from datetime import timedelta
-import numpy as np
+
+import pyproj
+'''
+transfrom the lon and lat to x and y
+'''
+def p1_2_p2(df,p1_str = 'epsg:4326', p2_str = 'epsg:3395'):
+    p1 = pyproj.Proj(init = p1_str)
+    p2 = pyproj.Proj(init = p2_str)
+    lon = df.longitude.values
+    lat = df.latitude.values
+    x1, y1 = p1(lon, lat)
+    x2, y2 = pyproj.transform(p1, p2, x1, y1, radians=True)
+    df['x'] = x2
+    df['y'] = y2
+    return df
+
 
 def st_dbscan(df, spatial_threshold, temporal_threshold, min_neighbors):
     """
     Python st-dbscan implementation.
     INPUTS:
         df={o1,o2,...,on} Set of objects
+        please make sure the first column is x, second is y, third is time
+        
         spatial_threshold = Maximum geographical coordinate (spatial) distance
         value
         temporal_threshold = Maximum non-spatial distance value
@@ -20,89 +37,55 @@ def st_dbscan(df, spatial_threshold, temporal_threshold, min_neighbors):
 
     # initialize each point with unmarked
     df['cluster'] = unmarked
-
+    df['index'] = range(df.shape[0])
+    m = df.values
     # for each point in database
-    for index, point in df.iterrows():
-        if df.loc[index]['cluster'] == unmarked:
-            neighborhood = retrieve_neighbors(index, df, spatial_threshold,
+    for index in range(m.shape[0]):
+        if m[index, 3] == unmarked:
+            neighborhood = retrieve_neighbors(index, m, spatial_threshold,
                                               temporal_threshold)
 
             if len(neighborhood) < min_neighbors:
-                df.at[index, 'cluster'] = noise
+                m[index, 3] = noise
             else:  # found a core point
                 cluster_label += 1
                 # assign a label to core point
-                df.at[index, 'cluster'] = cluster_label
+                m[index,3] = cluster_label
 
                 # assign core's label to its neighborhood
                 for neig_index in neighborhood:
-                    df.at[neig_index, 'cluster'] = cluster_label
+                    m[neig_index, 3] = cluster_label
                     stack.append(neig_index)  # append neighborhood to stack
 
                 # find new neighbors from core point neighborhood
                 while len(stack) > 0:
                     current_point_index = stack.pop()
                     new_neighborhood = retrieve_neighbors(
-                        current_point_index, df, spatial_threshold,
+                        current_point_index, m, spatial_threshold,
                         temporal_threshold)
 
                     # current_point is a new core
                     if len(new_neighborhood) >= min_neighbors:
                         for neig_index in new_neighborhood:
-                            neig_cluster = df.loc[neig_index]['cluster']
+                            neig_cluster = m[neig_index,3]
                             if any([neig_cluster == noise,
                                     neig_cluster == unmarked]):
-                                df.at[neig_index, 'cluster'] = cluster_label
+                                m[neig_index,3] = cluster_label
                                 stack.append(neig_index)
-    return df
+    return m
 
 
-def retrieve_neighbors(index_center, df, spatial_threshold, temporal_threshold):
+def retrieve_neighbors(index_center, m, spatial_threshold, temporal_threshold):
     neigborhood = []
-
-    center_point = df.loc[index_center]
+    center_point = m[index_center,:]
 
     # filter by time
-    min_time = center_point['date_time'] - timedelta(seconds=temporal_threshold)
-    max_time = center_point['date_time'] + timedelta(seconds=temporal_threshold)
-    df = df[(df['date_time'] >= min_time) & (df['date_time'] <= max_time)]
-
+    min_time = center_point[2] - timedelta(seconds=temporal_threshold)
+    max_time = center_point[2] + timedelta(seconds=temporal_threshold)
+    m = m[(m[:,2] >= min_time) & (m[:,2] <= max_time),:]
     # filter by distance
-    tmp = df.apply(lambda row: great_circle(
-        (center_point['latitude'], center_point['longitude']),
-        (row['latitude'], row['longitude'])), axis=1)
-    x = tmp.loc[:, ] <= spatial_threshold
-    neigborhood = x[x].index.values.tolist()
+    tmp = (m[:,0]-center_point[0])*(m[:,0]-center_point[0])+(m[:,1]-center_point[1])*(m[:,1]-center_point[1])
+    neigborhood = m[tmp <= (spatial_threshold*spatial_threshold),4].tolist()
     neigborhood.remove(index_center)
 
     return neigborhood
-
-
-def great_circle(a, b):
-    """Great-circle.
-
-    The great-circle distance or orthodromic distance is the shortest
-    distance between two points on the surface of a sphere, measured
-    along the surface of the sphere (as opposed to a straight line
-    through the sphere's interior).
-
-    :Note: use cython in the future
-    :returns: distance in meters.
-    """
-    import math
-    earth_radius = 6371.009
-    lat1, lng1 = np.radians(a[0]), np.radians(a[1])
-    lat2, lng2 = np.radians(b[0]), np.radians(b[1])
-
-    sin_lat1, cos_lat1 = np.sin(lat1), np.cos(lat1)
-    sin_lat2, cos_lat2 = np.sin(lat2), np.cos(lat2)
-
-    delta_lng = lng2 - lng1
-    cos_delta_lng, sin_delta_lng = np.cos(delta_lng), np.sin(delta_lng)
-
-    d = math.atan2(np.sqrt((cos_lat2 * sin_delta_lng) ** 2 +
-                           (cos_lat1 * sin_lat2 -
-                            sin_lat1 * cos_lat2 * cos_delta_lng) ** 2),
-                   sin_lat1 * sin_lat2 + cos_lat1 * cos_lat2 * cos_delta_lng)
-
-    return (earth_radius * d) * 1000
--- python/src/test_st_dbscan.py
+++ python/src/test_st_dbscan.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import pandas as pd
-import argparse
-from stdbscan import st_dbscan
+
+from stdbscan import st_dbscan, p1_2_p2
 
 def parse_dates(x):
     return datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f')
@@ -11,13 +11,16 @@ filename = 'sample.csv'
 
 def test_time():
     df = pd.read_csv(filename,sep=";",converters={'date_time': parse_dates})
-    result_t600 = st_dbscan(df, spatial_threshold=500, temporal_threshold=600, min_neighbors=5)
-
-    df = pd.read_csv(filename,sep=";",converters={'date_time': parse_dates})
-    result_t6 = st_dbscan(df, spatial_threshold=500, temporal_threshold=0.6, min_neighbors=5)
-
-    assert not result_t600.equals(result_t6)
-
+    '''
+    transfrom the lon and lat to x and y
+    need to selcet the right epsg
+    I don't the true epsg of sample, but get the same result by using epsg:4326 and epsg:32635
+    '''
+    df = p1_2_p2(df,p1_str = 'epsg:4326', p2_str = 'epsg:32635')
+    df_to_cluster = df[['x', 'y', 'date_time']]
+    result_t600 = st_dbscan(df_to_cluster.copy(), spatial_threshold=500, temporal_threshold=600, min_neighbors=5)
+    return result_t600
 
 if __name__ == '__main__':
-    test_time()
+    df = pd.DataFrame(test_time())
+    print(pd.value_counts(df[3]))
