35ade86e3fe6c46c2413cabc3cca7185531b17d0
Lucas Ponce <lucasmsp@gmail.com>
2019-03-01 10:58:40 -0300

    re-organizing

--- python/src/stdbscan.py
+++ python/src/stdbscan.py
@@ -1,91 +1,122 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from datetime import timedelta
-
 import pyproj
-'''
-transfrom the lon and lat to x and y
-'''
-def p1_2_p2(df,p1_str = 'epsg:4326', p2_str = 'epsg:3395'):
-    p1 = pyproj.Proj(init = p1_str)
-    p2 = pyproj.Proj(init = p2_str)
-    lon = df.longitude.values
-    lat = df.latitude.values
-    x1, y1 = p1(lon, lat)
-    x2, y2 = pyproj.transform(p1, p2, x1, y1, radians=True)
-    df['x'] = x2
-    df['y'] = y2
-    return df
 
 
-def st_dbscan(df, spatial_threshold, temporal_threshold, min_neighbors):
-    """
-    Python st-dbscan implementation.
-    INPUTS:
-        df={o1,o2,...,on} Set of objects
-        please make sure the first column is x, second is y, third is time
-        
-        spatial_threshold = Maximum geographical coordinate (spatial) distance
-        value
-        temporal_threshold = Maximum non-spatial distance value
-        min_neighbors = Minimun number of points within Eps1 and Eps2 distance
-    OUTPUT:
-        C = {c1,c2,...,ck} Set of clusters
-    """
-    cluster_label = 0
-    noise = -1
-    unmarked = 777777
-    stack = []
+class STDBSCAN(object):
 
-    # initialize each point with unmarked
-    df['cluster'] = unmarked
-    df['index'] = range(df.shape[0])
-    m = df.values
-    # for each point in database
-    for index in range(m.shape[0]):
-        if m[index, 3] == unmarked:
-            neighborhood = retrieve_neighbors(index, m, spatial_threshold,
-                                              temporal_threshold)
+    def __init__(self, col_lat, col_lon, col_time, spatial_threshold=500.0, 
+                 temporal_threshold=60.0, min_neighbors=15):
+        """
+        Python st-dbscan implementation.
 
-            if len(neighborhood) < min_neighbors:
-                m[index, 3] = noise
-            else:  # found a core point
-                cluster_label += 1
-                # assign a label to core point
-                m[index,3] = cluster_label
+        :param col_lat: Latitude column name;
+        :param col_lon:  Longitude column name;
+        :param col_time: Date time column name;
+        :param spatial_threshold: Maximum geographical coordinate (spatial)
+             distance value;
+        :param temporal_threshold: Maximum non-spatial distance value (meters);
+        :param min_neighbors: Minimum number of points within Eps1 and Eps2
+             distance;
+        """
+        self.col_lat = col_lat
+        self.col_lon = col_lon
+        self.col_time = col_time
+        self.spatial_threshold = spatial_threshold
+        self.temporal_threshold = temporal_threshold
+        self.min_neighbors = min_neighbors
 
-                # assign core's label to its neighborhood
-                for neig_index in neighborhood:
-                    m[neig_index, 3] = cluster_label
-                    stack.append(neig_index)  # append neighborhood to stack
+    def projection(self, df, p1_str='epsg:4326', p2_str='epsg:3395'):
+        """
+        Cython wrapper to converts from geographic (longitude,latitude)
+        to native map projection (x,y) coordinates. It needs to select the
+        right epsg. Values of x and y are given in meters
+        """
+        p1 = pyproj.Proj(init=p1_str)
+        p2 = pyproj.Proj(init=p2_str)
+        lon = df[self.col_lon].values
+        lat = df[self.col_lat].values
+        x1, y1 = p1(lon, lat)
+        x2, y2 = pyproj.transform(p1, p2, x1, y1, radians=True)
+        df[self.col_lon] = x2
+        df[self.col_lat] = y2
 
-                # find new neighbors from core point neighborhood
-                while len(stack) > 0:
-                    current_point_index = stack.pop()
-                    new_neighborhood = retrieve_neighbors(
-                        current_point_index, m, spatial_threshold,
-                        temporal_threshold)
+        print df
+        return df
 
-                    # current_point is a new core
-                    if len(new_neighborhood) >= min_neighbors:
-                        for neig_index in new_neighborhood:
-                            neig_cluster = m[neig_index,3]
-                            if any([neig_cluster == noise,
-                                    neig_cluster == unmarked]):
-                                m[neig_index,3] = cluster_label
-                                stack.append(neig_index)
-    return m
+    def _retrieve_neighbors(self, index_center, matrix):
 
+        center_point = matrix[index_center, :]
 
-def retrieve_neighbors(index_center, m, spatial_threshold, temporal_threshold):
-    neigborhood = []
-    center_point = m[index_center,:]
+        # filter by time
+        min_time = center_point[2] - timedelta(seconds=self.temporal_threshold)
+        max_time = center_point[2] + timedelta(seconds=self.temporal_threshold)
+        matrix = matrix[(matrix[:, 2] >= min_time) &
+                        (matrix[:, 2] <= max_time), :]
+        # filter by distance
+        tmp = (matrix[:, 0]-center_point[0])*(matrix[:, 0]-center_point[0]) + \
+            (matrix[:, 1]-center_point[1])*(matrix[:, 1]-center_point[1])
+        neigborhood = matrix[tmp <= (
+            self.spatial_threshold*self.spatial_threshold), 4].tolist()
+        neigborhood.remove(index_center)
 
-    # filter by time
-    min_time = center_point[2] - timedelta(seconds=temporal_threshold)
-    max_time = center_point[2] + timedelta(seconds=temporal_threshold)
-    m = m[(m[:,2] >= min_time) & (m[:,2] <= max_time),:]
-    # filter by distance
-    tmp = (m[:,0]-center_point[0])*(m[:,0]-center_point[0])+(m[:,1]-center_point[1])*(m[:,1]-center_point[1])
-    neigborhood = m[tmp <= (spatial_threshold*spatial_threshold),4].tolist()
-    neigborhood.remove(index_center)
+        return neigborhood
 
-    return neigborhood
+    def run(self, df):
+        """
+        INPUTS:
+            df={o1,o2,...,on} Set of objects;
+
+        OUTPUT:
+            C = {c1,c2,...,ck} Set of clusters
+        """
+        cluster_label = 0
+        noise = -1
+        unmarked = 777777
+        stack = []
+
+        # initial setup
+        df = df[[self.col_lon, self.col_lat, self.col_time]]
+        df = df.assign(cluster=unmarked)
+        df['index'] = range(df.shape[0])
+        matrix = df.values
+        df.drop(['index'], inplace=True, axis=1)
+
+        # for each point in database
+        for index in range(matrix.shape[0]):
+            if matrix[index, 3] == unmarked:
+                neighborhood = self._retrieve_neighbors(index, matrix)
+
+                if len(neighborhood) < self.min_neighbors:
+                    matrix[index, 3] = noise
+                else:  # found a core point
+                    cluster_label += 1
+                    # assign a label to core point
+                    matrix[index, 3] = cluster_label
+
+                    # assign core's label to its neighborhood
+                    for neig_index in neighborhood:
+                        matrix[neig_index, 3] = cluster_label
+                        stack.append(neig_index)  # append neighbors to stack
+
+                    # find new neighbors from core point neighborhood
+                    while len(stack) > 0:
+                        current_point_index = stack.pop()
+                        new_neighborhood = \
+                            self._retrieve_neighbors(current_point_index,
+                                                     matrix)
+
+                        # current_point is a new core
+                        if len(new_neighborhood) >= self.min_neighbors:
+                            for neig_index in new_neighborhood:
+                                neig_cluster = matrix[neig_index, 3]
+                                if any([neig_cluster == noise,
+                                        neig_cluster == unmarked]):
+                                    matrix[neig_index, 3] = cluster_label
+                                    stack.append(neig_index)
+
+        df['cluster'] = matrix[:, 3]
+        return df
+
--- python/src/test_st_dbscan.py
+++ python/src/test_st_dbscan.py
@@ -1,26 +1,56 @@
 from datetime import datetime
 import pandas as pd
+import numpy as np
+from stdbscan import STDBSCAN
 
-from stdbscan import st_dbscan, p1_2_p2
 
 def parse_dates(x):
     return datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f')
 
-filename = 'sample.csv'
+
+def plot_clusters(df, output_name):
+    import matplotlib.pyplot as plt
+
+    labels = df['cluster'].values
+    X = df[['longitude', 'latitude']].values
+
+    # Black removed and is used for noise instead.
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+              for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+
+        xy = X[class_member_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=6)
+
+    plt.title('ST-DSCAN: #n of clusters {}'.format(len(unique_labels)))
+    plt.show()
+    # plt.savefig(output_name)
 
 
 def test_time():
-    df = pd.read_csv(filename,sep=";",converters={'date_time': parse_dates})
+    filename = 'sample.csv'
+    df = pd.read_csv(filename, sep=";", converters={'date_time': parse_dates})
     '''
     transfrom the lon and lat to x and y
-    need to selcet the right epsg
-    I don't the true epsg of sample, but get the same result by using epsg:4326 and epsg:32635
+    need to select the right epsg
+    I don't the true epsg of sample, but get the same result by using 
+    epsg:4326 and epsg:32635
     '''
-    df = p1_2_p2(df,p1_str = 'epsg:4326', p2_str = 'epsg:32635')
-    df_to_cluster = df[['x', 'y', 'date_time']]
-    result_t600 = st_dbscan(df_to_cluster.copy(), spatial_threshold=500, temporal_threshold=600, min_neighbors=5)
+    st_dbscan = STDBSCAN(col_lat='latitude', col_lon='longitude',
+                         col_time='date_time', spatial_threshold=500,
+                         temporal_threshold=600, min_neighbors=5)
+    df = st_dbscan.projection(df, p1_str='epsg:4326', p2_str='epsg:32635')
+    result_t600 = st_dbscan.run(df)
     return result_t600
+
 
 if __name__ == '__main__':
     df = pd.DataFrame(test_time())
-    print(pd.value_counts(df[3]))
+    print(pd.value_counts(df['cluster']))
